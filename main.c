8b6bca4fd54785d30e6b81f0ec3d43da83637cf6
Katherine Silva <katherinesilva1406@gmail.com>
2026-05-13 10:55:55 -0400

    Primer commit de mi neurona

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "neural_network.h"
+
+int main () {
+    printf("SINA VISUAL NEURAL NETWORK\n");
+    return 0;
+}
---
+++ neural_network.h
@@ -0,0 +1,33 @@
+#ifndef NEURAL_NETWORK_H
+#define NEURAL_NETWORK_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 1. Estructura de la Neurona Individual
+typedef struct {
+    int num_entradas;
+    double *pesos;    // Arreglo dinámico en la RAM
+    double bias;
+    double salida;    // Resultado después de la activación
+    double delta;     // Se usará después para el aprendizaje (backpropagation)
+} Neurona;
+
+// 2. Estructura de una Capa (conjunto de neuronas)
+typedef struct {
+    int num_neuronas;
+    Neurona *neuronas; // Arreglo de estructuras tipo Neurona
+} Capa;
+
+// 3. Estructura Maestra de la Red Neuronal (SINA)
+typedef struct {
+    int num_capas;
+    Capa *capas;
+    double tasa_aprendizaje; // El "ritmo" al que ajustará los pesos
+} RedNeuronal;
+
+// Prototipos de funciones (las "promesas" de lo que programaremos después)
+RedNeuronal* inicializar_red(int num_capas, int *neuronas_por_capa, double tasa);
+void liberar_memoria(RedNeuronal *red);
+
+#endif
